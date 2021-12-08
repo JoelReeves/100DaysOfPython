edebacebe4769d20a7fb8172666ab05451aae1eb
joelreeves <jmrmb80@gmail.com>
2021-12-07 19:35:50 -0500

    Day 1 concepts

---
+++ BeginnerConcepts/main.py
@@ -0,0 +1,19 @@
+print("Hello World")
+print("Hello\nHow are you this morning?")
+
+# string concatenation
+print("Hello" + ", Joel")
+
+# using string interpolation
+name = "Jeanette Reeves"
+print(f"Good morning, {name}. I love you so much!")
+
+# getting user input
+name = input("What is your name? ")
+print("Hello, " + name)
+
+# Day 1 Project: Band Name Generator
+print("Hello! Welcome to the Band Name Generator")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name is " + city + " " + pet)
