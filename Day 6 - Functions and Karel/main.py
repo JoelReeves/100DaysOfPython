fb689cc1a326e19450b8adcbacd8606a0864c094
joelreeves <jmrmb80@gmail.com>
2021-12-14 23:30:03 -0500

    Day 6 concepts

---
+++ Day 6 - Functions and Karel/main.py
@@ -0,0 +1,12 @@
+# Function
+def print_name():
+    print("Hello, Joel")
+
+print_name()
+
+# While Loop
+age = 1
+while age < 18:
+    print(f"I am a child. I am {age} years old")
+    age += 1
+print("I'm finally 18 years old. I am an adult!")
