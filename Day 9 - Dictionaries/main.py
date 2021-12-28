552f658132fb833c5fe6eda42ff163770e932a09
joelreeves <jreeves@urbn.com>
2021-12-28 11:38:22 -0500

    Finishing Day 9

--- Day 9 - Dictionaries/main.py
+++ Day 9 - Dictionaries/main.py
@@ -1,63 +1,63 @@
 # Dictionaries
 
-# state_capitals = {
-#     "NJ": "Trenton",
-#     "PA": "Harrisburg",
-#     "OR": "Salem",
-#     "HI": "Honolulu"
-# }
-#
-# # getting an item from the dictionary
-# print(state_capitals["NJ"])
-#
-# # adding an item to the dictionary
-# state_capitals["CA"] = "Sacramento"
-#
-# # iterating through dictionary
-# for key in state_capitals:
-#     print(key, '->', state_capitals[key])
-#
-# for item in state_capitals.items():
-#     print(item)
-#
-# for key, value in state_capitals.items():
-#     print(key, '->', value)
+state_capitals = {
+    "NJ": "Trenton",
+    "PA": "Harrisburg",
+    "OR": "Salem",
+    "HI": "Honolulu"
+}
+
+# getting an item from the dictionary
+print(state_capitals["NJ"])
+
+# adding an item to the dictionary
+state_capitals["CA"] = "Sacramento"
+
+# iterating through dictionary
+for key in state_capitals:
+    print(key, '->', state_capitals[key])
+
+for item in state_capitals.items():
+    print(item)
+
+for key, value in state_capitals.items():
+    print(key, '->', value)
 
 # Grading Program - convert scores to grades
 # Scores 91 - 100: Grade = "Outstanding"
 # Scores 81 - 90: Grade = "Exceeds Expectations"
 # Scores 71 - 80: Grade = "Acceptable"
 # Scores 70 or lower: Grade = "Fail"
-# student_scores = {
-#     "Harry": 81,
-#     "Ron": 78,
-#     "Hermione": 99,
-#     "Draco": 74,
-#     "Neville": 62,
-# }
-#
-# def get_student_grade(score):
-#     if score >= 91 and score <= 100:
-#         return "Outstanding"
-#     elif score >= 81 and score <= 90:
-#         return "Exceeds Expectations"
-#     elif score >= 71 and score <= 80:
-#         return "Acceptable"
-#     else:
-#         return "Fail"
-#
-# student_grades = {}
-#
-# for key, value in student_scores.items():
-#     student_grades[key] = get_student_grade(value)
-#
-# print(student_grades)
-#
-# # Nesting a List in a Dictionary
-# travel_log = {
-#     "France": ["Paris, Dijon", "Lille"],
-#     "Italy": ["Rome", "Venice", "Florence"]
-# }
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+
+def get_student_grade(score):
+    if score >= 91 and score <= 100:
+        return "Outstanding"
+    elif score >= 81 and score <= 90:
+        return "Exceeds Expectations"
+    elif score >= 71 and score <= 80:
+        return "Acceptable"
+    else:
+        return "Fail"
+
+student_grades = {}
+
+for key, value in student_scores.items():
+    student_grades[key] = get_student_grade(value)
+
+print(student_grades)
+
+# Nesting a List in a Dictionary
+travel_log = {
+    "France": ["Paris, Dijon", "Lille"],
+    "Italy": ["Rome", "Venice", "Florence"]
+}
 
 # Dictionary in List
 # Writing a program that adds to a travel_log - a List that contains 2 Dictionaries
@@ -74,10 +74,49 @@ travel_log = [
 },
 ]
 
-#TODO: Write the function that will allow new countries to be added to the travel_log. 👇
 def add_new_country(country, visits, cities):
-    print()
+    new_country = {
+      "country": country,
+      "visits": visits,
+      "cities": cities
+    }
 
+    travel_log.append(new_country)
 
 add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
 print(travel_log)
+
+# Secret Auction
+import os
+from art import logo
+print(logo)
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def get_highest_bidder(bidders):
+    highest_bidder = ""
+    highest_bid = 0
+
+    for key, value in bidders.items():
+        if (value > highest_bid):
+            highest_bidder = key
+            highest_bid = value
+
+    print(f"The winner is {highest_bidder} with a bid of ${highest_bid}")
+
+bidders = {}
+end_bidding = False
+
+while not end_bidding:
+    name = input("What is your name? ")
+    price = int(input("What is your bid price? $"))
+    bidders[name] = price
+
+    continue_game = input("Are there other bidders? y or n: ").lower()
+
+    if continue_game == "y":
+        clear_console()
+    else:
+        end_bidding = True
+        get_highest_bidder(bidders)
